5499abcfb3334fab01d8cf163ec3a8d325323ac9
changjin <ckdwls9460@naver.com>
2020-06-02 14:15:27 +0900

    Initial Commit

---
+++ SwiftTemp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftTemp
+//
+//  Created by 하창진 on 2020/06/02.
+//  Copyright © 2020 하창진. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
